cf24af0737509e396012765190bba75aa06467a2
Arsenio Benitez <Arseniobenitezjr@hotmail.com>
2020-10-05 15:01:01 -0400

    _http

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,11 +6,11 @@ import {map} from 'rxjs/operators';
 @Injectable()
 
 export class ProductService {
-  private _albumURL: string = '../assests/album.json';
-  constructor(private http: HttpClient) { }
+  private _albumURL: string = '../assets/album.json';
+  constructor(private _http: HttpClient) { }
 
   getAlbum(id:number){
-    return this.http.get<string>(this._albumURL, {responseType:'json'});
+    return this._http.get<string>(this._albumURL, {responseType:'json'});
   }
 
 }
